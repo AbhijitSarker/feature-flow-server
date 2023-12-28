c9d14ed7c435b9ca5c3b77f245cc2eab6d122f36
AbhijitSarker <abhijitsarker03@gmail.com>
2023-12-28 23:10:45 +0600

    get all feature and update feature route added

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -3,8 +3,16 @@ const featureSchema = require('../models/featureSchema');
 const Feature = new mongoose.model("Feature", featureSchema);
 
 
-const getAllFeatures = (req, res) => {
-    res.send('All Features');
+const getAllFeatures = async (req, res) => {
+    try {
+        const features = await Feature.find({})
+        res.status(200).json({ features });
+    } catch {
+        res.status(500).json({
+            error: "There was a server side error!",
+        });
+    }
+
 }
 const getFeature = (req, res) => {
     res.json({ id: req.params.id });
@@ -25,9 +33,21 @@ const createFeature = async (req, res) => {
     }
 
 }
-const updateFeature = (req, res) => {
-    res.send('updateFeatures');
-}
+const updateFeature = async (req, res) => {
+    const { id: featureId } = req.params;
+
+    const feature = await Feature.findOneAndUpdate({ _id: featureId }, req.body, {
+        new: true,
+        runValidators: true,
+    })
+
+    if (!feature) {
+        return next(createCustomError(`No Feature with id : ${featureId}`, 404))
+    }
+
+    res.status(200).json({ feature })
+};
+
 const deleteFeature = (req, res) => {
     res.send('deleteFeature');
 }
