509457a6e4a5ede0f26d18f89bd7257d1a33dda0
AbhijitSarker <abhijitsarker03@gmail.com>
2024-01-02 19:57:12 +0600

    upvote and downvote Feature api created

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { getAllFeatures, getFeature, updateFeature, deleteFeature, createFeature, searchFeatures } = require('../controllers/mainControllers');
+const { upvoteFeature, downvoteFeature } = require('../controllers/voteController');
 
 router.route('/').get(searchFeatures, getAllFeatures).post(createFeature)
 router.route('/:id').get(getFeature).patch(updateFeature).delete(deleteFeature)
+
+// Increase vote count by feature ID
+router.put('/:id/upvote', upvoteFeature);
+
+// Decrease vote count by feature ID
+router.put('/:id/downvote', downvoteFeature);
+
 module.exports = router;
