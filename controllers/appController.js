6f6ee907acca4beba0037596d4ba0ce6e1b4e3df
AbhijitSarker <abhijitsarker03@gmail.com>
2024-01-06 13:55:15 +0600

    update app info route added

--- controllers/appController.js
+++ controllers/appController.js
@@ -3,7 +3,12 @@ const AppSchema = require('../models/appSchema');
 const App = new mongoose.model("App", AppSchema);
 
 const getAppInfo = async (req, res) => {
-
+    try {
+        const appInfo = await App.find();
+        res.status(200).json({ appInfo });
+    } catch (err) {
+        res.status(500).json({ error: 'There was a server side error!' });
+    }
 }
 const createAppInfo = async (req, res) => {
     try {
@@ -20,7 +25,21 @@ const createAppInfo = async (req, res) => {
     }
 }
 const updateAppInfo = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedInfo = await App.findByIdAndUpdate(id, req.body, {
+            new: true,
+            runValidators: true,
+        });
 
+        if (!updatedInfo) {
+            return res.status(404).json({ message: 'info not found' });
+        }
+
+        res.status(200).json({ message: 'info updated successfully!', appInfo: updatedInfo });
+    } catch (err) {
+        res.status(500).json({ error: 'There was a server side error!' });
+    }
 }
 
 
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAppInfo, createAppInfo, updateAppInfo } = require('../controllers/appController');
 
-router.route('/').get(getAppInfo).post(createAppInfo).patch(updateAppInfo)
-
+router.route('/').get(getAppInfo).post(createAppInfo)
+router.route('/:id').patch(updateAppInfo)
 module.exports = router;
